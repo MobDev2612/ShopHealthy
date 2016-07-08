ef9fe95df30aa0f87ad063775888c32febc0c343
MobDev2612 <hariharanswamy@gmail.com>
2016-07-09 00:30:24 +0530

    update

--- app/src/main/java/com/shopfitt/android/Fragment/ContactUsFragment.java
+++ app/src/main/java/com/shopfitt/android/Fragment/ContactUsFragment.java
@@ -1,7 +1,10 @@
 package com.shopfitt.android.Fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,7 @@ import com.shopfitt.android.R;
  */
 public class ContactUsFragment extends Fragment {
 
+    private View view;
 
     public ContactUsFragment() {
         // Required empty public constructor
@@ -24,7 +28,38 @@ public class ContactUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        view.findViewById(R.id.contact_us_email).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+        view.findViewById(R.id.contact_us_phone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doCall();
+            }
+        });
+    }
+
+    private void sendMail(){
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{ "hearmeout@shopfitt.in"});
+        email.putExtra(Intent.EXTRA_SUBJECT, "ShopFitt Android App");
+        //need this to prompts email client only
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+    }
+
+    private void doCall(){
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+919986644863"));
+        startActivity(intent);
+    }
 }
